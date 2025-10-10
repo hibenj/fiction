90cf49dad53b1a56700520f1b09ead96d2e2c469
benjamin <benjamin.hien@web.de>
2025-10-10 10:28:12 +0200

    :art: added L-inverter

--- include/fiction/technology/molecular_qca_library.hpp
+++ include/fiction/technology/molecular_qca_library.hpp
@@ -14,12 +14,14 @@
 #include <mockturtle/traits.hpp>
 #include <phmap.h>
 
+#include <stdexcept>
+
 namespace fiction
 {
 
 /**
- * A concrete molQCA gate library was proposed by the Politecnico di Torino and is based on simulations using the SCERPA
- * tool. Tiles in the molQCA library are \f$10 \times 10\f$ molQCA cells.
+ * This concrete molQCA gate library was proposed by the Politecnico di Torino and is based on physical simulations
+ * using the SCERPA tool. Tiles in the molQCA library are \f$10 \times 10\f$ molQCA cells.
  */
 class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10>
 {
@@ -53,10 +55,6 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
                     {
                         return FANOUT_MAP.at(p);
                     }
-                    /*if (lyt.fanout_size(n) == 3)
-                    {
-                        return FAN_OUT_1_3;
-                    }*/
                 }
             }
             if constexpr (fiction::has_is_buf_v<GateLyt>)
@@ -205,20 +203,6 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
     // ************************** Gates ***************************
     // ************************************************************
 
-    static constexpr const fcn_gate CENTER_WIRE{cell_list_to_gate<char>(
-    {{
-        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'b', 'b', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'b', 'b', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'c', 'c', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'c', 'c', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '}
-    }})};
-
     static constexpr const fcn_gate STRAIGHT_INVERTER{cell_list_to_gate<char>(
     {{
         {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
@@ -233,19 +217,33 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
         {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '}
     }})};
 
-    /*static constexpr const fcn_gate BENT_INVERTER{cell_list_to_gate<char>(
+    static constexpr const fcn_gate BENT_INVERTER{cell_list_to_gate<char>(
     {{
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', 'x', 'x', 'x', 'x'},
-        {' ', ' ', ' ', ' ', ' ', ' ', 'x', 'x', 'x', 'x'},
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'a', 'a', 'b', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'a', 'a', 'b', ' ', 'd', 'd'},
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', 'c', 'c', 'd'},
+        {' ', ' ', ' ', ' ', 'b', 'b', ' ', 'c', 'c', 'd'},
+        {' ', ' ', ' ', ' ', 'b', 'b', 'b', ' ', 'd', 'd'},
+        {' ', ' ', ' ', ' ', 'b', 'b', 'b', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+    }})};
+
+    static constexpr const fcn_gate BENT_INVERTER_R{cell_list_to_gate<char>(
+    {{
+        {' ', ' ', ' ', 'd', 'd', 'd', 'd', ' ', ' ', ' '},
+        {' ', ' ', ' ', 'd', 'c', 'c', 'd', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'c', 'c', ' ', ' ', ' ', ' '},
+        {' ', ' ', 'b', 'b', ' ', ' ', 'b', 'b', ' ', ' '},
+        {' ', ' ', 'b', 'b', 'b', 'a', 'a', 'a', 'a', 'a'},
+        {' ', ' ', 'b', 'b', 'b', 'a', 'a', 'a', 'a', 'a'},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-    }})};*/
+    }})};
 
     static constexpr const fcn_gate CONJUNCTION{cell_list_to_gate<char>(
     {{
@@ -387,20 +385,6 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
         {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '}
     }})};
 
-    /*static constexpr const fcn_gate FAN_OUT_1_3{cell_list_to_gate<char>(
-    {{
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', 'x', 'x', ' ', ' ', ' ', ' '}
-    }})};*/
-
     // ************************************************************
     // ************************** Wires ***************************
     // ************************************************************
@@ -431,6 +415,20 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+    }})};
+
+    static constexpr const fcn_gate CENTER_WIRE{cell_list_to_gate<char>(
+    {{
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'a', 'a', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'b', 'b', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'b', 'b', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'c', 'c', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'c', 'c', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', 'd', 'd', ' ', ' ', ' ', ' '}
     }})};
 
     static constexpr const fcn_gate CENTER_BENT_WIRE{cell_list_to_gate<char>(
@@ -491,9 +489,7 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
         {{{port_position(0, 5)}, {port_position(5, 9)}}, rotate_180(CENTER_BENT_WIRE_R)},
         {{{port_position(5, 9)}, {port_position(0, 5)}}, rotate_180(CENTER_BENT_WIRE)},
         {{{port_position(4, 0)}, {port_position(0, 5)}}, rotate_270(CENTER_BENT_WIRE_R)},
-        {{{port_position(0, 5)}, {port_position(4, 0)}}, rotate_270(CENTER_BENT_WIRE)}
-        // TODO more wires go here!
-    };
+        {{{port_position(0, 5)}, {port_position(4, 0)}}, rotate_270(CENTER_BENT_WIRE)}};
     /**
      * Lookup table for inverter rotations. Maps ports to corresponding inverters.
      */
@@ -512,7 +508,16 @@ class molecular_qca_library : public fcn_gate_library<mol_qca_technology, 10, 10
         {{{}, {port_position(5, 9)}}, STRAIGHT_INVERTER},
         {{{}, {port_position(0, 5)}}, rotate_90(STRAIGHT_INVERTER)},
         {{{}, {port_position(4, 0)}}, rotate_180(STRAIGHT_INVERTER)},
-        {{{}, {port_position(9, 4)}}, rotate_270(STRAIGHT_INVERTER)}};
+        {{{}, {port_position(9, 4)}}, rotate_270(STRAIGHT_INVERTER)},
+        // bent inverter
+        {{{port_position(4, 0)}, {port_position(9, 4)}}, BENT_INVERTER},
+        {{{port_position(9, 4)}, {port_position(4, 0)}}, BENT_INVERTER_R},
+        {{{port_position(9, 4)}, {port_position(5, 9)}}, rotate_90(BENT_INVERTER)},
+        {{{port_position(5, 9)}, {port_position(9, 4)}}, rotate_90(BENT_INVERTER_R)},
+        {{{port_position(0, 5)}, {port_position(5, 9)}}, rotate_180(BENT_INVERTER_R)},
+        {{{port_position(5, 9)}, {port_position(0, 5)}}, rotate_180(BENT_INVERTER)},
+        {{{port_position(4, 0)}, {port_position(0, 5)}}, rotate_270(BENT_INVERTER_R)},
+        {{{port_position(0, 5)}, {port_position(4, 0)}}, rotate_270(BENT_INVERTER)}};
     /**
      * Lookup table for conjunction rotations. Maps ports to corresponding AND gates.
      */
